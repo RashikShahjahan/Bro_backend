8591d0b3b4612b8a6c308b424b891ae895ab632c
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2017-07-21 11:34:12 -0400

    JSON web token

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,11 +4,17 @@ from django.conf.urls import url
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 from nlp_core import views
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
     
     url(r'^admin/', admin.site.urls),
     url(r'^data/', views.DataList.as_view()),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
